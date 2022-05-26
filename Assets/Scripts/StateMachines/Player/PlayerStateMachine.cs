fc6c33a67c322e161f9e5f940eea94c7fa11c626
Lorenzo Cherubin <lorenzo.cherubin@gmail.com>
2022-05-26 17:50:44 +0200

    Initial commit

---
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine :StateMachine
+{
+    // Start is called before the first frame update
+
+    [field: SerializeField]
+    public InputReader InputReader
+    {
+        get;
+        private set;
+    }
+    
+    [field: SerializeField]
+    public CharacterController Controller
+    {
+        get;
+        private set;
+    }
+    
+    [field: SerializeField]
+    public Animator Animator
+    {
+        get;
+        private set;
+    }
+    [field: SerializeField]
+    public float FreeLookMovementSpeed
+    {
+        get;
+        private set;
+    }
+    
+    private void Start()
+    {
+        SwitchState(new PlayerTestState(this));
+        
+        
+    }
+}
